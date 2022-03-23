cb5ec4763698103bbb5681f7448c26a78ffc5391
Joe <jliang7558@gmail.com>
2022-03-23 15:56:39 -0400

    rendered settings component

--- api/serializers.py
+++ api/serializers.py
@@ -10,3 +10,12 @@ class CreateRoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
         fields = ('guest_can_pause','votes_to_skip')
+
+class UpdateRoomSerializer(serializers.ModelSerializer):
+    #every time code passed in will already belong to a room so it's not 'unique'
+    #updating a code means it already exists
+    #redefine code field so it doesn't have to be unique
+    code = serializers.CharField(validators=[])
+    class Meta:
+        model = Room
+        fields = ('guest_can_pause','votes_to_skip','code')
--- frontend/src/components/Room.js
+++ frontend/src/components/Room.js
@@ -1,6 +1,7 @@
 import React, { useState, useRef, useEffect } from "react";
 import { useParams, useNavigate, Navigate } from 'react-router-dom';
 import { Grid, Button, Typography } from "@material-ui/core";
+import CreateRoomPage from "./CreateRoomPage";
 
 function Room(props) {
     const { roomCode } = useParams(); //must use with react router dom v6, only compatible with function component
@@ -9,6 +10,7 @@ function Room(props) {
     const [votesToSkip, setVotesToSkip] = useState(2);
     const [guestCanPause, setGuestCanPause] = useState(false);
     const [isHost, setIsHost] = useState(false);
+    const [showSettings, setShowSettings] = useState(false);
 
     const getRoomDetails = () => {
         fetch('/api/get-room'+'?code='+roomCode)
@@ -35,8 +37,43 @@ function Room(props) {
             history('/');
         });
     }
+    const updateShowSettings = (value) => {
+        setShowSettings(value);
+    }
+    let RenderSettingsButton = () => {
+        if(isHost){
+            return (
+                <Grid item xs={12} align="center">
+                    <Button variant="contained" color="primary" onClick={() => updateShowSettings(true)}>Settings</Button>
+                </Grid>
+            ); 
+        }
+        return null      
+    }
+    let RenderSettings = () => {
+        return(
+            <Grid container spacing={1}>
+                <Grid item xs={12} align="center">
+                    <CreateRoomPage
+                        update={true}
+                        votesToSkip={votesToSkip}
+                        guestCanPause={guestCanPause}
+                        roomCode={roomCode}
+                        updateCallback={true}
+                    />
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <Button variant="contained" color="secondary" onClick={() => updateShowSettings(false)}>Close</Button>
+                </Grid>
+            </Grid>
+        );
+    }
 
     getRoomDetails()
+
+    if(showSettings) {
+        return <RenderSettings />
+    }
     return (
         <Grid container spacing={1}>
             <Grid item xs={12} align="center">
@@ -51,6 +88,7 @@ function Room(props) {
             <Grid item xs={12} align="center">
                 <Typography variant="h6" component="h6">Host: {isHost.toString()}</Typography>
             </Grid>
+            <RenderSettingsButton />
             <Grid item xs={12} align="center">
                 <Button color="secondary" variant="contained" onClick={leaveButtonPressed}>Leave Room</Button>
             </Grid>
