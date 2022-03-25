dcfa4f3c0cab50694a94807249dece172af9b700
Joe <jliang7558@gmail.com>
2022-03-25 17:38:36 -0400

    increased token length

--- spotify/models.py
+++ spotify/models.py
@@ -4,8 +4,8 @@ from api.models import Room
 class SpotifyToken(models.Model):
     user = models.CharField(max_length=50, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    refresh_token = models.CharField(max_length=150)
-    access_token = models.CharField(max_length=150)
+    refresh_token = models.CharField(max_length=1000)
+    access_token = models.CharField(max_length=1000)
     expires_in = models.DateTimeField()
     token_type = models.CharField(max_length=50)
 
